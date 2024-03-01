03aa8ca3b218ccd81d2332a062de8708f7cddc15
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-01 22:24:19 +0500

    Add files via upload

---
+++ international_error.ts
@@ -0,0 +1,12 @@
+//If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+// creating array
+let newArray :string[] = ['Will Smith' , 'James Bond' , 'Tom Cruise' ];
+
+// accessing array by index
+console.log(newArray[5]);
+
+//make sure you correct the error before closing the program.
+
+//correct the array
+console.log(newArray);
