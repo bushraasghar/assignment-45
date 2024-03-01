1b36a5b5a93938521263a91c051c632acdc49f7c
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-01 22:21:31 +0500

    Add files via upload

--- new_guest_list.js
+++ new_guest_list.js
@@ -6,27 +6,27 @@ let Guest_List = ['sana', 'umaima', 'mahnoor'];
 let absent_Guest = 'sana';
 let new_Guest = 'yamna';
 Guest_List[0] = new_Guest;
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThanks!\n\n');
-}
-console.log(`Ms.  ${absent_Guest} is not coming to the party.`);
-console.log('Good news! We arranged a big table so we are inviting 3 more guests.');
+//for (let i = 0; i < Guest_List.length; i++) {
+//  console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThanks!\n\n');
+//}
+//console.log(`Ms.  ${absent_Guest} is not coming to the party.`);
+//console.log('Good news! We arranged a big table so we are inviting 3 more guests.')
 Guest_List.unshift('Hoor');
 Guest_List.splice(2, 0, 'Bushra');
 Guest_List.push('maham');
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThanks!\n\n');
-}
-console.log('\nSorry we cannot arranged a big table, only two people are invited.');
+//for (let i = 0; i < Guest_List.length; i++) {
+//   console.log('Dear Ms. ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThanks!\n\n');
+//}
+//console.log('\nSorry we cannot arranged a big table, only two people are invited.');
 while (Guest_List.length > 2) {
     let remove_Guest = Guest_List.pop();
-    console.log(`Sorry Ms. ${remove_Guest}, you are not invied for Dinner.  `);
+    //console.log(`Sorry Ms. ${remove_Guest}, you are not invied for Dinner.  `)
 }
-//guest removed
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Ms. ' + Guest_List[i] + ',\n\nYor are still invited for Dinner.\n\nThanks!\n\n');
-}
-
-//Removed some guests.
+//for (let i = 0; i < Guest_List.length; i++) {
+//  console.log('Dear Ms. ' + Guest_List[i] + ',\n\nYor are still invited for Dinner.\n\nThanks!\n\n');
+//}
 Guest_List.splice(0, 2);
 console.log(Guest_List);
+//Exercise 19
+//print a message indicating the number of people you are inviting to dinner.
+console.log(`total number of guest are: ${Guest_List.length}`);
