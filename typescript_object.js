d363b1ebb128ab47fa16f53d3c38cd054403fe7d
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-01 22:23:40 +0500

    Add files via upload

---
+++ typescript_object.js
@@ -0,0 +1,9 @@
+"use strict";
+//Write a program that creates Objects containing these items.
+let person = {
+    age: 18,
+    name: 'Hoor',
+    nationality: 'pakistan',
+    student: true
+};
+console.log(person);
---
+++ typescript_object.ts
@@ -0,0 +1,23 @@
+
+//Write a program that creates Objects containing these items.
+
+// person_Name :string = 'Hoor';
+
+//const  personName_Array :string[] = ['person' , 'car' , 'Clod drink'];
+
+
+interface person{
+    age : number,
+    name : string,
+    nationality : string,
+    student : boolean,
+}
+
+let person :person = {
+    age : 18 ,
+    name : 'Hoor' ,
+    nationality : 'pakistan' ,
+    student : true
+}
+
+console.log(person);
