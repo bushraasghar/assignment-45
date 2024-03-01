133078dcf6644c3978e7125f73df9b9a445add94
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-01 22:16:19 +0500

    Add files via upload

---
+++ Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Hoor', 'Umaima', 'Mahnoor', 'Ajiyya'];
+const message = 'Meet my friend: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,9 @@
+let names = ['Hoor', 'Umaima', 'Mahnoor', 'Ajiyya'];
+
+const message :string = 'Meet my friend: '
+
+for (let i = 0; i < names.length; i++) {
+
+    console.log(message + names[i]);
+
+}
