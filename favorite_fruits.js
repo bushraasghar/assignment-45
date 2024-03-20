98a6bc921df7a703c70a46294472877e27396f58
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-20 22:40:23 +0500

    Add files via upload

---
+++ favorite_fruits.js
@@ -0,0 +1,21 @@
+"use strict";
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+let favorite_fruits = ['kiwi', 'mango', 'orange'];
+if (favorite_fruits.includes('mango')) {
+    console.log('I really like mango!');
+}
+if (favorite_fruits.includes('orange')) {
+    console.log('I really like orange!');
+}
+if (favorite_fruits.includes('kiwi')) {
+    console.log('I really like kiwi!');
+}
+if (favorite_fruits.includes('stawbary')) {
+    console.log('I really like stawbary!');
+}
+if (favorite_fruits.includes('apple')) {
+    console.log('I really like apple!');
+}
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement,
+// such as You really like bananas!
---
+++ favorite_fruits.ts
@@ -0,0 +1,26 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement,
+// such as You really like bananas!
+
+let favorite_fruits: string[] = ['kiwi' , 'mango' , 'orange'];
+if(favorite_fruits.includes('mango')){
+    console.log('I really like mango!');
+}
+  
+if(favorite_fruits.includes('orange')){
+    console.log('I really like orange!');
+}
+
+if(favorite_fruits.includes('kiwi')){
+    console.log('I really like kiwi!');
+}
+
+if(favorite_fruits.includes('stawbary')){
+    console.log('I really like stawbary!')
+}
+
+if(favorite_fruits.includes('apple')){
+    console.log('I really like apple!');
+}
