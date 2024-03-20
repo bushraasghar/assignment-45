841909a0e7bb0998681af5572e873b5bfa37cbd2
Bushra Asghar <159794215+bushraasghar@users.noreply.github.com>
2024-03-20 22:50:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+let personName = '';
+personName = prompt('what is your name?') || '';
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, would you like to learn some Python today?`);
+}
+else {
+    alert('you have to fill your name !');
+}
